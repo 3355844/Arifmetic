f8e8b8d5f13c4e23783fe9510df2ec24a4c5b9ce
Andrei Ponomarenko <3355844@gmail.com>
2018-07-03 20:24:47 +0300

    Arithmetic simple+=

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         int i = 0;
-        int i2 = 1;
-        int i3 = 2;
+        int i2 = 10;
+        int i3 = 20;
         String s = "String";
         String s1 = "String";
         String s2 = "other String";
@@ -17,11 +17,15 @@ public class Main {
         result = i * i2;
         System.out.println("Result: " + i + " * " + i2 + " = " + result);
         i++;
+        result = i3 % i;
+        System.out.println("Result: " + i3 + " % " + i + " = " + result);
         result = i / i3;
         System.out.println("Result: " + i + " / " + i3 + " = " + result);
         i++;
         System.out.println("i++: " + i++);
         System.out.println("++i: " + i++);
-        System.out.println("i: " + i--);
+        System.out.println("--i: " + --i);
+        i += -3;
+        System.out.println(i);
     }
 }
